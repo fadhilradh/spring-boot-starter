916fc259bcf2548f9e64ef58e5f795e3dd41052e
Akhmad <fadhil.akhmad@accenture.com>
2023-03-30 10:35:42 +0700

    add database connection

--- src/main/java/com/fadhilradh/Main.java
+++ src/main/java/com/fadhilradh/Main.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,9 +12,21 @@ import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
     @GetMapping("/greet")
